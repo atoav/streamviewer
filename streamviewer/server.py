300230bebea0829c5f3e2fdeae0262174ae1bd0f
David Huss <dh@atoav.com>
2021-02-25 11:10:04 +0100

    Refactoring

--- streamviewer/server.py
+++ streamviewer/server.py
@@ -14,36 +14,49 @@ app = Flask(APPLICATION_NAME, template_folder='../templates', static_folder="../
 
 # Initialize the configuration (create a default one if needed)
 config = initialize_config(app.logger)
+config["application"]["hls_path"] = config["application"]["hls_path"].rstrip("/")
 
-app.logger.info('Ready to take requests')
-
-
-
+app.logger.info("{} is ready to take requests: {}".format(APPLICATION_NAME, app.config['SERVER_NAME']))
 
 
-# This gets run for each request
-@app.route('/stream/<streamkey>', methods = ['GET'])
-def streamview(streamkey):
+
+
+
+
+@app.route('/streams/<streamkey>', methods = ['GET'])
+def stream(streamkey):
+    """
+    If there is a stream, display it, otherwise note that the stream is missing
+    """
     app.logger.info('200, Access to /{}'.format(streamkey))
-    hls_path = config["application"]["hls_path"].rstrip("/")
-    app.logger.info("Looking for {}/{}.m3u8".format(hls_path,  streamkey))
+    app.logger.debug("Looking for {}/{}.m3u8".format(hls_path,  streamkey))
+
+    # Strip potential trailing slashes
     streamkey = streamkey.rstrip("/")
+
+    # Get a list of the active streams
     active_streams = list_streams()
     active_streams = [str(s).rsplit("/")[-1].replace(".m3u8", "") for s in active_streams]
+
+    # Render a different Template if the stream is missing
     if streamkey not in active_streams:
-        return render_template("streammissing.html", application_name=APPLICATION_NAME, page_title=config["application"]["page_title"], streamkey=streamkey)
+        app.logger.warning("Looking for stream {}, but it didn't exist".format(streamkey))
+        return render_template("stream_missing.html", application_name=APPLICATION_NAME, page_title=config["application"]["page_title"], streamkey=streamkey)
     else:
         return render_template('stream.html', application_name=APPLICATION_NAME, page_title=config["application"]["page_title"], hls_path=hls_path, streamkey=streamkey)
 
+
 @app.route('/', methods = ['GET'])
-@app.route('/stream', methods = ['GET'])
-def streamlist():
-    app.logger.info('Listing streams')
+@app.route('/streams', methods = ['GET'])
+def streams():
+    """
+    List the streams
+    """
     active_streams = list_streams()
     active_streams = [str(s).rsplit("/")[-1].replace(".m3u8", "") for s in active_streams]
     hls_path = config["application"]["hls_path"].rstrip("/")
-    app.logger.info('Active streams: {}'.format(", ".join([str(s) for s in active_streams])))
-    return render_template('streamlist.html', application_name=APPLICATION_NAME, page_title=config["application"]["page_title"], active_streams=active_streams)
+    app.logger.info('Listing active streams: {}'.format(", ".join([str(s) for s in active_streams])))
+    return render_template('streams.html', application_name=APPLICATION_NAME, page_title=config["application"]["page_title"], active_streams=active_streams)
 
 
 
